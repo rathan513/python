4e10eae3ae023a06ef52134bd16753b6ab7705f0
rathan513 <162105131+rathan513@users.noreply.github.com>
2024-03-04 14:45:56 +0530

    Add files via upload

---
+++ 2_Operators_Questions.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# 1 .Question | Write a program to print 15,5,2 and 0 ,use the approriate operator
+
+a = 10
+b = 5
+
+# Your Answer
+print(a+b)
+print(a-b)
+print(a//b)
+print(b*0)
+
+
+
+
+
+
+
+# In[5]:
+
+
+# 2 .Question | write a code to print 9 , use the approriate operator
+a = 3
+b = 2
+
+# Your Answer
+print(a**b)
+
+
+# In[1]:
+
+
+# 4 .Question | # write a code to print 2 , 2.25 and 1 , use the approriate operator
+
+x = 9
+y = 4
+
+# Your Answer
+print(x//y)
+print(x/y)
+print(x%y)
+
+
+
+
+
+# In[2]:
+
+
+# 5 .Question |  # write a code print True ,use the approriate operator
+
+a = 10
+b = 5
+
+# Your Answer
+if(a/b):
+   print("true")
+else:
+    print("false")
+
+
+
+
+
+# In[9]:
+
+
+# 5 .Question | # # write a code print Fale
+
+a = 15
+b = 15 
+
+# Your Answer
+if a==b:
+    print("False")
+else:    
+    print("True")
+
+
+
+
+# In[10]:
+
+
+# 6 .Question | # # write a code print True
+
+a = 24
+b = 24
+
+# Your Answer
+if (a==b):
+    print("True")
+else:
+     print("false")
+
+
+
+
+
+# In[11]:
+
+
+# 7 .Question | # # write a code print True
+
+a = "python"
+b = "python"
+
+if a==b:
+    print("True")
+
+
+
+
+
+# In[14]:
+
+
+# 8 .Question | #  write a code print false using and operator
+
+a = 10 
+b = 20
+
+# Your Answer
+if a&b!=10&20:
+    print("true")
+else:
+    print("false")
+
+
+
+
+
+# In[16]:
+
+
+# 9 .Question | #  write a code print True using or operator
+
+a = 20
+b = 10 
+
+# Your Answer
+print(a<b or a>b)
+
+
+
+
+
+# In[17]:
+
+
+# 10 .Question | #  write a code print True using Not operator
+
+a = 20
+b = 10 
+
+# Your Answer
+print(a!=b)
+
+
+
+
